1b141bcbe80823741a5f9bfc27f2034e2534f234
Allef Rodrigues <allef.rodri@gmail.com>
2025-05-10 10:12:52 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/br/com/fiap/ecocontrol/EcocontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ecocontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/ecocontrol/EcocontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ecocontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
